8935d700427931d08431898a655964c1dc80583b
Andrew Pronek <102892202+okruntimeerror@users.noreply.github.com>
2023-02-23 21:42:46 -0500

    Update an375715_lab_6.c

--- lab_assignment_6/an375715_lab_6.c
+++ lab_assignment_6/an375715_lab_6.c
@@ -2,18 +2,18 @@
 
 int search(int numbers[], int low, int high, int value) {
     if (low > high) {
-        // base case: the value is not found in the array
+       
         return -1;
     }
     int mid = (low + high) / 2;
     if (numbers[mid] == value) {
-        // base case: the value is found at the middle index
+        
         return mid;
     } else if (numbers[mid] > value) {
-        // recursive case: search the left half of the array
+      
         return search(numbers, low, mid - 1, value);
     } else {
-        // recursive case: search the right half of the array
+        
         return search(numbers, mid + 1, high, value);
     }
 }
